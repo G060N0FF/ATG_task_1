ea1ced8896d6d591989ddf937a99a3f55b59cf2e
G060N0FF <georgi.hr.dim@gmail.com>
2021-07-30 00:10:19 +0300

    Search for users

--- ChatProject/App/views.py
+++ ChatProject/App/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
+from .forms import SearchForm
 
 
 # index view
@@ -11,30 +12,39 @@ def index(request):
     # a form to update the user information
     user_change_form = UserCreationForm()
 
-    user_copy = request.user.username
+    # a form to search for users
+    user_search_form = SearchForm()
+
+    found_users = []
 
     # if a form has been filled
     if request.method == 'POST':
-        # get the filled form
-        user_change_form = UserCreationForm(data=request.POST, instance=request.user)
-
         # check if the user has been changed
-        if user_change_form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password1']
-            if not User.objects.filter(username=username).exists():
-                request.user.username = username;
-                request.user.set_password(password)
-                request.user.save()
+        if 'username' in request.POST:
+            user_change_form = UserCreationForm(data=request.POST, instance=request.user)
+            if user_change_form.is_valid():
+                username = request.POST['username']
+                password = request.POST['password1']
+                if not User.objects.filter(username=username).exists():
+                    request.user.username = username;
+                    request.user.set_password(password)
+                    request.user.save()
 
-                login(request, request.user)
+                    login(request, request.user)
 
-            return redirect('/')
+        # check if the user has searched for other users
         else:
-            request.user.username = user_copy
-            request.user.save()
+            user_search_form = SearchForm(request.POST)
+            if user_search_form.is_valid():
+                query = request.POST['query']
+                found_users = User.objects.filter(username__icontains=query)
 
-    context = {'user': request.user.username, 'user_change_form': user_change_form}
+    context = {
+        'user': request.user.username,
+        'user_change_form': user_change_form,
+        'user_search_form': user_search_form,
+        'found_users': found_users
+    }
     return render(request, 'App/index.html', context)
 
 
